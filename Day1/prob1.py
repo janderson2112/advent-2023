6c01df4952c4a9c3032b87877a61d2022953571a
JaminGame <Jacob21anderson@gmail.com>
2023-12-01 14:58:53 -0500

    Day 1 Problem 2 - partial

--- Day1/prob1.py
+++ Day1/prob1.py
@@ -3,6 +3,5 @@ with open('input.txt') as input:
     for line in input:
         line = line.rstrip()
         intList = [char for char in line if char.isnumeric()]
-        val = int(intList[0] + intList[-1])
-        sum += val
+        sum += int(intList[0] + intList[-1])
 print(sum)
--- Day1/prob2.py
+++ Day1/prob2.py
@@ -1,3 +1,37 @@
+import re
+
+def replaceNum(num):
+    if num.isnumeric():
+        return num
+    else:
+        match num:
+            case 'one':
+                return '1'
+            case 'two':
+                return '2'
+            case 'three':
+                return '3'
+            case 'four':
+                return '4'
+            case 'five':
+                return '5'
+            case 'six':
+                return '6'
+            case 'seven':
+                return '7'
+            case 'eight':
+                return '8'
+            case 'nine':
+                return '9'
+
+sum = 0
+strList = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 with open('input.txt') as input:
     for line in input:
         line = line.rstrip()
+        print(line)
+        regex = re.compile(r'[0-9]|one|two|three|four|five|six|seven|eight|nine')
+        numList = regex.findall(line)
+        sum += int(replaceNum(numList[0]) + replaceNum(numList[-1]))
+        print(replaceNum(numList[0]) + replaceNum(numList[-1]))
+print(sum)
