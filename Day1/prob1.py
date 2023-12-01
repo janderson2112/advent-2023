5a815d1bdcf8f2e847cb9c8107295f7485a27e8b
JaminGame <Jacob21anderson@gmail.com>
2023-12-01 14:42:57 -0500

    Start prob 2, organize files

--- Day1/prob1.py
+++ Day1/prob1.py
@@ -1,5 +1,5 @@
 sum = 0
-with open('day1-1-input.txt') as input:
+with open('input.txt') as input:
     for line in input:
         line = line.rstrip()
         intList = [char for char in line if char.isnumeric()]
---
+++ Day1/prob2.py
@@ -0,0 +1,3 @@
+with open('input.txt') as input:
+    for line in input:
+        line = line.rstrip()
